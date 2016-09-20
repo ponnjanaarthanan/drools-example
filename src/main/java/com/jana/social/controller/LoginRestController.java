963ae2784f4eda4ef5dbf0f8438079765f02b41f
Ponn Janaarthanan Sundhararajan <Ponn Janaarthanan Sundhararajan>
2016-09-20 14:59:26 +0530

    Initial Commit

---
+++ src/main/java/com/jana/social/controller/LoginRestController.java
@@ -0,0 +1,19 @@
+package com.jana.social.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginRestController {
+	
+	@RequestMapping("/")
+	public String index(){
+		return "index";
+	}
+	
+	@RequestMapping("/login")
+	public String login(){
+		return "login";
+	}
+
+}
---
+++ src/main/java/com/jana/social/security/WebSecurity.java
@@ -0,0 +1,14 @@
+package com.jana.social.security;
+
+public class WebSecurity{
+	/*
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.antMatcher("/**")
+			.authorizeRequests()
+			.antMatchers("/", "/login**", "/webjars/**")
+			.permitAll().anyRequest()
+			.authenticated();
+	}
+	*/
+}
